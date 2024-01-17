c13f7261875680675c0c943596a8b3a26753fe1d
Garrett Lang <garrett@softserv.me>
2024-01-17 12:20:12 -0500

    removed other languages - only kept english

--- src/language-provider/entries/it-it.js
+++
@@ -1,9 +0,0 @@
-import itMessages from '../locales/it-it.json';
-
-const ItLang = {
-	messages: {
-		...itMessages,
-	},
-	locale: 'it-IT',
-};
-export default ItLang;
--- src/language-provider/entries/vi-vn.js
+++
@@ -1,9 +0,0 @@
-import viMessages from '../locales/vi-vn.json';
-
-const VnLang = {
-	messages: {
-		...viMessages,
-	},
-	locale: 'vi-VN',
-};
-export default VnLang;
