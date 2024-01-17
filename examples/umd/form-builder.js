7b708a93cc3f802d762673d3dedee037645c004d
Garrett Lang <garrett@softserv.me>
2024-01-17 12:18:40 -0500

    removed docs and examples since they are outdated based on changed made

--- examples/umd/form-builder.js
+++
@@ -1,6 +0,0 @@
-/* eslint-disable no-undef */
-// const e = React.createElement;
-const SurveyBuilder = ReactSurveyBuilder.ReactSurveyBuilder;
-const domContainer = document.querySelector('#form-builder');
-
-ReactDOM.render(e(SurveyBuilder, { url: '/api/formdata', saveUrl: '/api/formdata' }), domContainer);
